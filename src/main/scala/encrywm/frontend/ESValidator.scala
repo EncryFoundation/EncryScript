274ecb7912c2ed40b3c5cc588dbc9dc3cd1a8f38
Ilya Oskin <ilya.arcadich@gmail.com>
2018-03-28 12:58:10 +0300

    ESValidator improvements.

--- src/main/scala/encrywm/frontend/ESValidator.scala
+++ src/main/scala/encrywm/frontend/ESValidator.scala
@@ -10,6 +10,15 @@ object ESValidator {
 
   type ValidationResult = Either[ValidationFailure, ValidationSuccess.type]
 
+  def validateSource(source: String): ValidationResult = {
+    ESPreprocessor.process(source) match {
+      case Failure(e) =>
+        Left(ValidationFailure(e.getMessage))
+      case Success(_) =>
+        Right(ValidationSuccess)
+    }
+  }
+
   def validateFromFile(path: String): ValidationResult = {
     if (!path.endsWith(".esc")) return Left(ValidationFailure("Wrong file extension."))
     val source = scala.io.Source.fromFile(path).mkString
