c4e550c8efb3b3f33af100a457ef87ead473e0a3
Ilya Oskin <ilya.arcadich@gmail.com>
2018-03-28 09:22:31 +0300

    Preprocessor introduced.

---
+++ src/main/scala/encrywm/frontend/Preprocessor.scala
@@ -0,0 +1,24 @@
+package encrywm.frontend
+
+import encrywm.ast.Ast.TREE_ROOT.Contract
+import encrywm.frontend.parser.Statements
+import encrywm.frontend.semantics.StaticAnalyser
+import encrywm.ast.AstCodec._
+import fastparse.all._
+
+import scala.util.Try
+
+object Preprocessor {
+
+  type SerializedContract = Array[Byte]
+
+  def process(s: String): Try[Contract] = Try {
+    val parsed = (Statements.contract ~ End).parse(s).get.value
+    StaticAnalyser.scan(parsed)
+    parsed
+  }
+
+  def processAndSerialize(s: String): Try[SerializedContract] = process(s).map { p =>
+    codec.encode(p).require.toByteArray
+  }
+}
