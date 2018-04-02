04ff61441f379875e7d1b5aa0ec4f5cbb75534b7
Ilya Oskin <ilya.arcadich@gmail.com>
2018-04-02 11:46:04 +0300

    Inheriting added to ESObject.

--- src/main/scala/encrywm/backend/env/ESObject.scala
+++ src/main/scala/encrywm/backend/env/ESObject.scala
@@ -1,11 +1,14 @@
 package encrywm.backend.env
 
-case class ESObject(name: String, attrs: Map[String, ESValue]) extends ESEnvComponent {
+case class ESObject(name: String,
+                    attrs: Map[String, ESValue],
+                    ancestorOpt: Option[ESObject] = None) extends ESEnvComponent {
 
   def getAttr(n: String): Option[ESValue] = attrs.get(n)
+    .orElse(ancestorOpt.flatMap(_.getAttr(n)))
 
   override def equals(obj: scala.Any): Boolean = obj match {
-    case ESObject(thatName, thatAttrs) =>
+    case ESObject(thatName, thatAttrs, _) =>
       thatName == this.name && thatAttrs.zip(this.attrs).forall { case ((_, v1), (_, v2)) => v1 == v2 }
     case _ => false
   }
