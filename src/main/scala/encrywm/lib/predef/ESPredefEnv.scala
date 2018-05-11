0e9cc2911f5a12fc63a3db40f1a5fca1a524a43f
aleksandr <alexbromel@gmail.com>
2018-05-11 16:22:47 +0300

    Base58Decode impl

--- src/main/scala/encrywm/lib/predef/ESPredefEnv.scala
+++ src/main/scala/encrywm/lib/predef/ESPredefEnv.scala
@@ -1,6 +1,7 @@
 package encrywm.lib.predef
 
 import encrywm.backend.env.ESEnvComponent
+import encrywm.lib.predef.functions.decode.ESPredefDecode
 import encrywm.lib.predef.functions.hash.ESPredefHash
 import encrywm.lib.predef.functions.signature.ESPredefSignature
 import encrywm.lib.predef.functions.time.ESPredefTime
@@ -10,5 +11,6 @@ object ESPredefEnv {
   val predefFunctions: Map[String, ESEnvComponent] =
     ESPredefTime.predefFunctions ++
     ESPredefHash.predefFunctions ++
-    ESPredefSignature.predefFunctions
+    ESPredefSignature.predefFunctions ++
+    ESPredefDecode.predefFunctions
 }
