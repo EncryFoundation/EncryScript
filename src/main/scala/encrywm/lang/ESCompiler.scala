cb07ad77b3fe7f33332691342e5ea188d4cc8052
Ilya Oskin <ilya.arcadich@gmail.com>
2018-06-02 12:07:54 +0300

    ESCompiler refactoring.

--- src/main/scala/encrywm/lang/ESCompiler.scala
+++ src/main/scala/encrywm/lang/ESCompiler.scala
@@ -21,35 +21,38 @@ object ESCompiler {
     */
   def compile(source: String): Try[Contract] = {
     val comps: Array[String] = source.split(Lexer.SchemaSeparator)
-    if (comps.length > 1) {
-      encrytl.common.SourceProcessor.process(comps.head).flatMap { schemas =>
-        Parser.parse(comps.last).flatMap { parsedScript =>
-          schemas.foldLeft[Try[Seq[Types.ESTypedObject]]](Success(Seq.empty[Types.ESTypedObject])) {
-            case (Success(acc), schema) =>
-              Converter.schema2ESType(schema) match {
-                case Success(obj) => Success(acc :+ obj)
-                case Failure(e) => Failure(e)
-              }
-            case (failure @ Failure(_), _) => failure
-          }.flatMap { types =>
-            StaticProcessor(TypeSystem(types)).process(parsedScript).map { res =>
-              Transformer.transform(SchemaBinder.bind(new Optimizer().optimize(res), schemas)) match {
-                case c: Contract => c
-                case other => throw new Exception(s"Unexpected node type: $other")
-              }
+    if (comps.length > 1) compileWithSchema(comps.head, comps.last)
+    else compileScript(source)
+  }
+
+  private def compileScript(scriptSource: String): Try[Contract] =
+    Parser.parse(scriptSource).flatMap { parsedScript =>
+      StaticProcessor.default.process(parsedScript).map { res =>
+        Transformer.transform(new Optimizer().optimize(res)) match {
+          case c: Contract => c
+          case other => throw new Exception(s"Unexpected node type: $other")
+        }
+      }
+    }
+
+  private def compileWithSchema(schemaSource: String, scriptSource: String): Try[Contract] =
+    encrytl.common.SourceProcessor.process(schemaSource).flatMap { schemas =>
+      Parser.parse(scriptSource).flatMap { parsedScript =>
+        schemas.foldLeft[Try[Seq[Types.ESTypedObject]]](Success(Seq.empty[Types.ESTypedObject])) {
+          case (Success(acc), schema) =>
+            Converter.schema2ESType(schema) match {
+              case Success(obj) => Success(acc :+ obj)
+              case Failure(e) => Failure(e)
+            }
+          case (failure @ Failure(_), _) => failure
+        }.flatMap { types =>
+          StaticProcessor(TypeSystem(types)).process(parsedScript).map { res =>
+            Transformer.transform(SchemaBinder.bind(new Optimizer().optimize(res), schemas)) match {
+              case c: Contract => c
+              case other => throw new Exception(s"Unexpected node type: $other")
             }
           }
         }
       }
-    } else {
-      Parser.parse(source).flatMap { parsedScript =>
-        StaticProcessor.default.process(parsedScript).map { res =>
-          Transformer.transform(new Optimizer().optimize(res)) match {
-            case c: Contract => c
-            case other => throw new Exception(s"Unexpected node type: $other")
-          }
-        }
-      }
     }
-  }
 }
